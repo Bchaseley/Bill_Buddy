f8069375e5731fc689ba91707c26b2bdb29eafa2
Alec Embke <aembke@gmail.com>
2022-03-12 18:20:41 -0800

    stash

--- server/routes/app.routes.js
+++ server/routes/app.routes.js
@@ -1,59 +1,19 @@
-const TransController = require("../controllers/trans.controller");
-const UserController = require("../controllers/user.controller");
+const transactions = require("../controllers/transactions");
+const user = require("../controllers/users");
+const jwt = require("../middleware/jwt");
 
 module.exports = app => {
 
     //all routes for users
-    app.post("/register", (req, res) => {
-        console.log(req.body);
-        UserController.register(req.body)
-            .then(response => {
-                res.status(200).send(response);
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
-    });
-    
-    app.post("/login", (req, res) => {
-        UserController.login(req.body)
-            .then(response => {
-                res.status(200).send(response);
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
-    });
+    app.post("/register", transactions.register);
+    app.post("/login", user.login);
 
-    //all routes for transactions
-    app.get('/api/allTrans', (req, res) => {
-        TransController.getTrans()
-            .then(response => {
-                res.status(200).send(response);
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
-    })
+    app.use("/api/*", jwt.authenticate)
 
-    app.post('/api/trans', (req, res) => {
-        TransController.addTrans(req.body)
-            .then(response => {
-                res.status(200).send(response);
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
-    });
+    app.get("/api/transactions", transactions.read);
+    app.post("/api/transaction", transactions.create);
+    app.put("/api/transaction", transactions.update);
+    app.delete("/api/transaction", transactions.delete);
 
-    app.delete('/api/trans/:id', (req, res) => {
-        TransController.deleteTrans(req.params.id)
-            .then(response => {
-                res.status(200).send(response);
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
-    });
-
+    app.get("/api/user/:id", user.read);
 }
